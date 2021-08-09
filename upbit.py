07f5fbf94479adec8296b5755c3e0303e266c7f7
pororobro <50974407+pororobro@users.noreply.github.com>
2021-08-09 23:51:31 +0900

    Update upbit.py

--- upbit.py
+++ upbit.py
@@ -28,6 +28,7 @@ def get_balance(ticker):
                 return 0
     return 0
 
+
 def get_current_price(ticker):
     """현재가 조회"""
     return pyupbit.get_orderbook(tickers=ticker)[0]["orderbook_units"][0]["ask_price"]
